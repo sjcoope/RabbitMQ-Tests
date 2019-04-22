d3b9082e92239cffcdd9a45ecc5715f5884082f3
Simon Coope <simon.coope@sjcnet.co.uk>
2019-04-22 18:07:49 +1000

    Workqueues

---
+++ Receive/Receive.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receive
+{
+    public class Receive
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using(var connection = factory.CreateConnection())
+            {
+                using(var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                    );
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, eventArgs) => 
+                    {
+                        var body = eventArgs.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine($"Received {message}");
+                    };
+                    channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+
+                    Console.WriteLine("Console still open...");
+                    Console.Read();
+                }
+            }
+        }
+    }
+}
---
+++ Worker/Worker.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Threading;
+
+namespace Worker
+{
+    public class Worker
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(
+                        queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null
+                    );
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("Received {0}", message);
+
+                        int dots = message.Split('.').Length - 1;
+                        Thread.Sleep(dots * 1000);
+
+                        Console.WriteLine("Done");
+                    };
+                    channel.BasicConsume(queue: "task_queue", autoAck: true, consumer: consumer);
+
+                    Console.WriteLine("Console still open...");
+                    Console.Read();
+                }
+            }
+        }
+    }
+}
